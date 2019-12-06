936a93966457ac81f2efbb6bdabb94cda54e7093
sai teja <saitejasunkari04@gmail.com>
2019-12-06 13:19:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/ui/addEmployee/AddEmployeeFragment.java
@@ -0,0 +1,21 @@
+package com.example.group13_f2019_mad3125_fp.ui.addEmployee;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.group13_f2019_mad3125_fp.R;
+
+public class AddEmployeeFragment extends Fragment {
+
+
+
+    }
---
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/ui/home/HomeFragment.java
@@ -0,0 +1,20 @@
+package com.example.group13_f2019_mad3125_fp.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.group13_f2019_mad3125_fp.R;
+
+public class HomeFragment extends Fragment {
+
+
+}
