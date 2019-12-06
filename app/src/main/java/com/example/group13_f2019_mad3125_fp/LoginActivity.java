8d954e2ce11e4733d41afb1de37e2e2312a476c8
sai teja <saitejasunkari04@gmail.com>
2019-12-06 14:53:41 -0500

    working on splash

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.group13_f2019_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void onLoginClick(View view) {
     }
 }
