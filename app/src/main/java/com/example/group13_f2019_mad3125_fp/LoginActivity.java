ac2e863ad4eee06f0d508903e8ceb22cc651e78b
jeenapaul <jeenapaul1014@gmail.com>
2019-12-06 14:53:00 -0500

    object created for onclick

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.group13_f2019_mad3125_fp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
