744088bd8c6cc8a0e409454cb63b02b8323b78fb
jeenapaul <jeenapaul1014@gmail.com>
2019-12-06 14:34:59 -0500

    Onclick Method Declared

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
@@ -11,4 +11,5 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+    public void onLoginClick(View view){
 }
