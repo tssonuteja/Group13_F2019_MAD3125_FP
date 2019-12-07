9b21ae283dcd8436d2fe6d8ec73847927a5da39b
sai teja <saitejasunkari04@gmail.com>
2019-12-06 20:58:00 -0500

    validating the login screen

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText Password;
     private TextView Info;
     private Button Login;
-    private int counter = 5;
+    //private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
         Name = (EditText) findViewById(R.id.et_email);
         Password = (EditText) findViewById(R.id.et_password);
-        //Info = (TextView) findViewById(R.id.tvInfo);
+        Info = (TextView) findViewById(R.id.tvInfo);
         Login = (Button) findViewById(R.id.btn_login);
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,47 +41,47 @@ public class LoginActivity extends AppCompatActivity {
 
 
         private void validate(String userName, String userPassword) {
-            if ((userName.equals("Admin")) && (userPassword.equals("1234"))) {
+            if ((userName.equals("user@employee.com")) && (userPassword.equals("s3cr3t"))) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
 
 
-            if(userName.isEmpty() && userPassword.equals("1234"))
+            if(userName.isEmpty() && userPassword.equals("s3cr3t"))
             {
                 Name.setError("empty");
-                //Info.setText("Username is empty");
+                Info.setText("Username is empty");
             }
-            else if(userName.equals("Admin") && userPassword.isEmpty()) {
+            else if(userName.equals("user@employee.com") && userPassword.isEmpty()) {
                 Password.setError("empty");
-               // Info.setText("Password is empty");
+               Info.setText("Password is empty");
             }
 
-            else if(userName.equals("Admin") && userPassword!="1234"){
+            else if(userName.equals("user@employee.com") && userPassword!="s3cr3t"){
                 Password.setError("invalid password");
-                //Info.setText("Invalid password");
+                Info.setText("Invalid password");
             }
-            else if(userName!="Admin"&& userPassword.equals("1234")){
+            else if(userName!="user@employee.com"&& userPassword.equals("s3cr3t")){
                 Name.setError("invalid username");
-               // Info.setText("Invalid username");
+                Info.setText("Invalid username");
             }
 
 
-            else if(userName!="Admin" || userPassword!="1234"){
+            else if(userName!="user@employee.com" || userPassword!="s3cr3t"){
 
                 Name.setError("invalid username");
                 Password.setError("invalid password");
 
-                //Info.setError("invalid credentials");
-               // Info.setText("invalid credentials");
+               // Info.setError("invalid credentials");
+                Info.setText("invalid credentials");
 
 
 
             }
 
             else{
-                //Info.setText("Success");
+                Info.setText("Success");
             }
 
 
