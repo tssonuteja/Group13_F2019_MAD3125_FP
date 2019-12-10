b00ae968a802c5dbc8423b17f3da9e08c00b473b
jeenapaul <jeenapaul1014@gmail.com>
2019-12-09 20:02:06 -0500

    working on payrollAdapter

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollAdapter.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollAdapter.java
@@ -1,4 +1,31 @@
 package com.example.group13_f2019_mad3125_fp;
 
-public class PayrollAdapter {
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PayrollAdapter extends RecyclerView.Adapter<PayrollAdapter.ViewHolder> {
+    private Context context;
+    private List<PayrollData> payrollDataList;
+    private OnPayrollClicked onPayrollClicked;
+
+    @NonNull
+    @Override
+    public PayrollAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PayrollAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
