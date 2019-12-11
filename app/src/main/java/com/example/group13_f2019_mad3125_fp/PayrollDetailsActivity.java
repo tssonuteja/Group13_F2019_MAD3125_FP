e80f0884bc79e69b9e874fb96e524d636a785968
sai teja <saitejasunkari04@gmail.com>
2019-12-10 23:26:16 -0500

    added earnings for payrolldetailsActivity.xml

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
@@ -13,5 +13,14 @@ public class PayrollDetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payroll_details);
+
+        tv_job_three = findViewById(R.id.tv_job_three);
+        tv_job_two = findViewById(R.id.tv_job_two);
+        tv_job_one = findViewById(R.id.tv_job_one);
+        tv_job_type = findViewById(R.id.tv_job_type);
+        tv_vehicle = findViewById(R.id.tv_vehicle);
+        tv_earning = findViewById(R.id.tv_earning);
+        tv_name = findViewById(R.id.tv_name);
+
     }
 }
