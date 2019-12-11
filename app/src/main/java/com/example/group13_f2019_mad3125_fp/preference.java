7f44bf206b7c8149f135048ee65a54b8356706af
sai teja <saitejasunkari04@gmail.com>
2019-12-11 07:24:35 -0500

    initializing a constructor

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/preference.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/preference.java
@@ -1,9 +1,14 @@
 package com.example.group13_f2019_mad3125_fp;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 
 public class preference {
 
     private SharedPreferences preferences;
+
+    public Preference(Context context){
+        preferences = context.getSharedPreferences("myPreference", Context.MODE_PRIVATE);
+    }
 
 }
