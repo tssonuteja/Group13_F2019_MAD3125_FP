a0d3198e2bffe4e8b25ac3cfc84898d573db72af
jeenapaul <jeenapaul1014@gmail.com>
2019-12-10 23:49:43 -0500

    Using Getter Setter Method

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/Employee.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/Employee.java
@@ -6,6 +6,44 @@ public class Employee {
     private String earning;
     private Vehicle vehicle;
     private Job job;
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEarning() {
+        return earning;
+    }
+
+    public void setEarning(String earning) {
+        this.earning = earning;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
 
 }
