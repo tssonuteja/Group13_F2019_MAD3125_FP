a03f975260d9ce09b5dacd684fe44d0514e0e9b0
jeenapaul <jeenapaul1014@gmail.com>
2019-12-10 22:19:30 -0500

    Declared the variable

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollAdapter.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollAdapter.java
@@ -1,3 +1,24 @@
+package com.example.group13_f2019_mad3125_fp;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.List;
+
+
+
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PayrollAdapter extends RecyclerView.Adapter<PayrollAdapter.ViewHolder> {
+    private Context context;
+    private List<Employee> employeeList;
+    private OnPayrollClicked onPayrollClicked;
+
+
+}
