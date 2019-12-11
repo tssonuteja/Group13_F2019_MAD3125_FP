29bcb62d9a2a167c2e3dfa0f3e59af63f87ee6a1
jeenapaul <jeenapaul1014@gmail.com>
2019-12-10 23:51:36 -0500

    create fields for class Job

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/Job.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/Job.java
@@ -1,4 +1,14 @@
 package com.example.group13_f2019_mad3125_fp;
 
 public class Job {
+    private String jobType;
+    private String partTimeType;
+    private double salary;
+    private double bonus;
+    private String school;
+    private double rate;
+    private double hours;
+    private double commission;
+    private double fixed;
+
 }
