dea88b096f195bfad6c01435cc80f6b2843115ab
sai teja <saitejasunkari04@gmail.com>
2019-12-10 23:23:49 -0500

    working on payrolldetailsActivity

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
@@ -3,8 +3,11 @@ package com.example.group13_f2019_mad3125_fp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PayrollDetailsActivity extends AppCompatActivity {
+    Employee employee;
+    TextView tv_job_one, tv_job_two, tv_job_three, tv_job_type, tv_vehicle, tv_earning, tv_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
