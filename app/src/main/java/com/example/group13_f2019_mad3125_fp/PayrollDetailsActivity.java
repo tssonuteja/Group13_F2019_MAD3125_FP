1c21557200937e9bca72863910a0a76f436ca239
sai teja <saitejasunkari04@gmail.com>
2019-12-10 22:58:09 -0500

    added PayrollDetailsactivity

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/PayrollDetailsActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PayrollDetails extends AppCompatActivity {
+public class PayrollDetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
