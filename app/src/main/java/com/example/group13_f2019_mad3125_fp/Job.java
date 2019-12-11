586af9cbfe56d01b0ee572d7ec54360d2d52e848
jeenapaul <jeenapaul1014@gmail.com>
2019-12-10 23:52:21 -0500

    Using Getter And Setter Method

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/Job.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/Job.java
@@ -10,5 +10,76 @@ public class Job {
     private double hours;
     private double commission;
     private double fixed;
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public double getCommission() {
+        return commission;
+    }
+
+    public void setCommission(double commission) {
+        this.commission = commission;
+    }
+
+    public double getFixed() {
+        return fixed;
+    }
+
+    public void setFixed(double fixed) {
+        this.fixed = fixed;
+    }
+
+    public String getPartTimeType() {
+        return partTimeType;
+    }
+
+    public void setPartTimeType(String partTimeType) {
+        this.partTimeType = partTimeType;
+    }
 
 }
