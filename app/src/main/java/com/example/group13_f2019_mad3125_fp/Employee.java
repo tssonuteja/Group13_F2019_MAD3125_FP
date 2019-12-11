0bba81a459f9085462cd5a4fd46c5d48f45dcdfe
jeenapaul <jeenapaul1014@gmail.com>
2019-12-10 23:48:36 -0500

    Declared All fields

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/Employee.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/Employee.java
@@ -1,4 +1,11 @@
 package com.example.group13_f2019_mad3125_fp;
 
 public class Employee {
+    private String name;
+    private int age;
+    private String earning;
+    private Vehicle vehicle;
+    private Job job;
+
+
 }
