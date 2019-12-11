c30429f2f44e74afa4a1f7f8a5be25ab66e9eef5
sai teja <saitejasunkari04@gmail.com>
2019-12-11 09:00:27 -0500

    working on payrolllist

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/ui/listPayroll/ListPayrollFragment.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/ui/listPayroll/ListPayrollFragment.java
@@ -1,27 +1,28 @@
 package com.example.group13_f2019_mad3125_fp.ui.listPayroll;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.group13_f2019_mad3125_fp.PayrollAdapter;
+import com.example.group13_f2019_mad3125_fp.PayrollDetailsActivity;
 import com.example.group13_f2019_mad3125_fp.R;
+import com.example.group13_f2019_mad3125_fp.interfaces.OnPayrollClicked;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ListPayrollFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ListPayrollFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import static com.example.group13_f2019_mad3125_fp.MainActivity.employeeList;
+
+
+
 public class ListPayrollFragment extends Fragment {
 
     private RecyclerView payroll_list;
@@ -34,76 +35,21 @@ public class ListPayrollFragment extends Fragment {
         return root;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ListPayrollFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ListPayrollFragment newInstance(String param1, String param2) {
-        ListPayrollFragment fragment = new ListPayrollFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private void setAdapter() {
+
+        PayrollAdapter adapter = new PayrollAdapter(getActivity(), employeeList, new OnPayrollClicked() {
+            @Override
+            public void onPayrollClicked(int position) {
+                Intent intent = new Intent(getActivity(), PayrollDetailsActivity.class);
+                intent.putExtra("position", position);
+                startActivity(intent);
+            }
+        });
+
+        LinearLayoutManager manager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+        payroll_list.setLayoutManager(manager);
+        payroll_list.setAdapter(adapter);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_payroll, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
+
