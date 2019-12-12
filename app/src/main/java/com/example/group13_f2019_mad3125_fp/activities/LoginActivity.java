d3d4898551f4824c75aec5c8da5bb751e2d6b978
jeenapaul <jeenapaul1014@gmail.com>
2019-12-11 22:18:25 -0500

    Error cleared

--- app/src/main/java/com/example/group13_f2019_mad3125_fp/activities/LoginActivity.java
+++ app/src/main/java/com/example/group13_f2019_mad3125_fp/activities/LoginActivity.java
@@ -1,94 +1,65 @@
 package com.example.group13_f2019_mad3125_fp.activities;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.group13_f2019_mad3125_fp.Preference;
 import com.example.group13_f2019_mad3125_fp.R;
+
+import static android.text.TextUtils.isEmpty;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText Name;
-    private EditText Password;
-    private TextView Info;
-    private Button Login;
-    //private int counter = 5;
+    private String username, password;
+    EditText et_email, et_password;
+    Preference preference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        preference = new Preference(this);
 
+        username = "user@employee.com";
+        password = "s3cr3t";
 
-        Name = (EditText) findViewById(R.id.et_email);
-        Password = (EditText) findViewById(R.id.et_password);
-        Info = (TextView) findViewById(R.id.tvInfo);
-        Login = (Button) findViewById(R.id.btn_login);
-        Login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                validate(Name.getText().toString(), Password.getText().toString());
-
-            }
-        });
+        et_email = findViewById(R.id.et_email);
+        et_password = findViewById(R.id.et_password);
     }
 
-
-
-
-        private void validate(String userName, String userPassword) {
-            if ((userName.equals("user@employee.com")) && (userPassword.equals("s3cr3t"))) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+    public void onLoginClick(View view){
+        String sEmail = et_email.getText().toString();
+        String sPswd = et_password.getText().toString();
+        if (isEmpty(sEmail) || isEmpty(sPswd)){
+            Toast.makeText(this, "Fields cannot be empty", Toast.LENGTH_SHORT).show();
+        } else {
+            if (!Patterns.EMAIL_ADDRESS.matcher(sEmail).matches()){
+                et_email.setError("Invalid email");
+            } else {
+                if (sEmail.equals(username) && sPswd.equals(password)){
+                    loginSuccess();
+                } else {
+                    Toast.makeText(this, "Invalid email/password", Toast.LENGTH_SHORT).show();
+                    et_email.requestFocus();
+                }
             }
-
-
-            if(userName.isEmpty() && userPassword.equals("s3cr3t"))
-            {
-                Name.setError("empty");
-                Info.setText("Username is empty");
-            }
-            else if(userName.equals("user@employee.com") && userPassword.isEmpty()) {
-                Password.setError("empty");
-               Info.setText("Password is empty");
-            }
-
-            else if(userName.equals("user@employee.com") && userPassword!="s3cr3t"){
-                Password.setError("invalid password");
-                Info.setText("Invalid password");
-            }
-            else if(userName!="user@employee.com"&& userPassword.equals("s3cr3t")){
-                Name.setError("invalid username");
-                Info.setText("Invalid username");
-            }
-
-
-            else if(userName!="user@employee.com" || userPassword!="s3cr3t"){
-
-                Name.setError("invalid username");
-                Password.setError("invalid password");
-
-               // Info.setError("invalid credentials");
-                Info.setText("invalid credentials");
-
-
-
-            }
-
-            else{
-                Info.setText("Success");
-            }
-
-
         }
+
+    }
+
+    private void loginSuccess() {
+
+        preference.put("isLoggedIn", true);
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
 }
-
-
-
